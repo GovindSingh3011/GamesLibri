738502a5ee32fe0138acc7ed625f9a71ef621b12
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-01-08 14:05:06 +0530

    install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Home from './pages/Home.jsx'
+import Home from './components/Home.jsx'
+import FetchGames from './components/FetchGames.jsx'
+import FetchAllGames from './components/FetchAllGames.jsx'
+import Category from './components/Category.jsx'
+import Search from './components/Search.jsx'
+import About from './components/About.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Home />} />
+      <Route path="/category" element={<Category />} />
+      <Route path="/category/:genre" element={<FetchGames />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/allgames" element={<FetchAllGames />} />
+      <Route path="/about" element={<About />} />
     </Route>
   )
 )
