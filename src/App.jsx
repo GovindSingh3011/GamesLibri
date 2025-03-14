b46adfc019ea09a31f1d1f82037fa8481cf90848
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-03-14 15:09:48 +0530

    Implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import FetchGames from "./components/FetchGames.jsx";
+import FetchAllGames from "./components/FetchAllGames.jsx";
+import Category from "./components/Category.jsx";
+import Search from "./components/Search.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
-    <>
-    <Header />
-    <Outlet/>
-    <Footer/>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/category/:genre" element={<FetchGames />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/allgames" element={<FetchAllGames />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import Home from './components/Home.jsx'
-import FetchGames from './components/FetchGames.jsx'
-import FetchAllGames from './components/FetchAllGames.jsx'
-import Category from './components/Category.jsx'
-import Search from './components/Search.jsx'
-import About from './components/About.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />} />
-      <Route path="/category" element={<Category />} />
-      <Route path="/category/:genre" element={<FetchGames />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/allgames" element={<FetchAllGames />} />
-      <Route path="/about" element={<About />} />
-    </Route>
-  )
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
